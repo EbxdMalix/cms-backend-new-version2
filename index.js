57cb23a51b7789dca4a3175ef14f202635fadbca
EbxdMalix <76601476+EbxdMalix@users.noreply.github.com>
2026-07-05 00:58:43 +0500

    updated.index.js
    edewd

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const crypto = require("crypto");
 const rateLimit = require("express-rate-limit");
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 
 // ============================================
